28fd365e10aacf62cb35b52ef752307fdffb8e12
Oleksandr Tatarskyi <oleksandr.tatarskyi@gmail.com>
2022-12-03 19:35:53 +0200

    Change app title and image

--- landmarks/ContentView.swift
+++ landmarks/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "bird")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Landmarks")
+                .font(.largeTitle)
+                .foregroundColor(.accentColor)
+                .italic()
         }
         .padding()
     }
