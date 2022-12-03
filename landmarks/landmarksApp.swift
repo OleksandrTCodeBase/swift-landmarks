e9df6a4d4f82a8046a5e53449299aeb49344850b
Oleksandr Tatarskyi <oleksandr.tatarskyi@gmail.com>
2022-12-03 19:05:00 +0200

    init

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Oleksandr Tatarskyi on 03.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
