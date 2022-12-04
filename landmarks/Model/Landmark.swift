52c9dffe6f9c35cbe4b78233aee34f58dc5d3bd8
Oleksandr Tatarskyi <oleksandr.tatarskyi@gmail.com>
2022-12-04 13:36:27 +0200

    create landmark model

---
+++ landmarks/Model/Landmark.swift
@@ -0,0 +1,34 @@
+//
+//  Landmark.swift
+//  landmarks
+//
+//  Created by Oleksandr Tatarskyi on 04.12.2022.
+//
+
+import Foundation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
