29b4fb5d1be216842f5ae7118f69dae64ee51fa7
Oleksandr Tatarskyi <oleksandr.tatarskyi@gmail.com>
2022-12-04 13:03:20 +0200

    compose the detail view

--- landmarks/ContentView.swift
+++ landmarks/ContentView.swift
@@ -10,23 +10,41 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            VStack(alignment: .leading) {
-                Image(systemName: "bird")
-                    .imageScale(.large)
-                Text("Landmarks")
-                    .font(.largeTitle)
-                    .italic()
-                HStack {
-                    Text("Joshua Tree National Park")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("California")
-                        .font(.subheadline)
+            VStack {
+                MapView()
+                    .ignoresSafeArea(edges: .top)
+                    .frame(height: 300)
+                
+                CircleImage()
+                    .offset(y: -130)
+                    .padding(.bottom, -130)
+                
+                VStack(alignment: .leading) {
+                    Image(systemName: "bird")
+                        .imageScale(.large)
+                    Text("Landmarks")
+                        .font(.largeTitle)
+                        .italic()
+                    HStack {
+                        Text("Joshua Tree National Park")
+                        Spacer()
+                        Text("California")
+                           
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                    Divider()
+                    
+                    Text("About Turtle Rock")
+                        .font(.title2)
+                    Text("Description of the Tutrle Rock place")
                 }
             }
+            .padding()
+            .foregroundColor(.accentColor)
+            
+            Spacer()
         }
-        .padding()
-        .foregroundColor(.accentColor)
     }
 }
 
