c42a95a950796fd57719b4eb31a584a36303a370
Oleksandr Tatarskyi <oleksandr.tatarskyi@gmail.com>
2022-12-04 17:53:44 +0200

    create the list of landmarks

---
+++ landmarks/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  landmarks
+//
+//  Created by Oleksandr Tatarskyi on 04.12.2022.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
