0bd65213342834140da1fd87465204f9be3ceb5d
Oleksandr Tatarskyi <oleksandr.tatarskyi@gmail.com>
2022-12-05 12:23:55 +0200

    set up navigation between list and detail

---
+++ landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,55 @@
+//
+//  LandmarkDetail.swift
+//  landmarks
+//
+//  Created by Oleksandr Tatarskyi on 05.12.2022.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            VStack {
+                MapView()
+                    .ignoresSafeArea(edges: .top)
+                    .frame(height: 300)
+                
+                CircleImage()
+                    .offset(y: -130)
+                    .padding(.bottom, -130)
+                
+                VStack(alignment: .leading) {
+                    Image(systemName: "bird")
+                        .imageScale(.large)
+                    Text("Landmarks")
+                        .font(.largeTitle)
+                        .italic()
+                    HStack {
+                        Text("Joshua Tree National Park")
+                        Spacer()
+                        Text("California")
+                           
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                    Divider()
+                    
+                    Text("About Turtle Rock")
+                        .font(.title2)
+                    Text("Description of the Tutrle Rock place")
+                }
+            }
+            .padding()
+            .foregroundColor(.accentColor)
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
